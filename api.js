f3ecfcdf34850eb8ce292c07ea639c54152eb9f9
Jannis R <mail@jannisr.de>
2017-08-08 15:44:53 +0200

    refactor

--- api.js
+++ api.js
@@ -1,38 +1,24 @@
 'use strict'
 
-const express = require('express')
-const hsts = require('hsts')
-const compression = require('compression')
-const path = require('path')
-const serve = require('serve-static')
+const stats = require('./lib/stats')
+const index = require('./lib/page-index')
+const page = require('./lib/page')
+const pageHistory = require('./lib/page-history')
 
-const createApi = (dat) => {
-	const stats = require('./lib/stats')(dat)
-	const index = require('./lib/page-index')(dat)
-	const page = require('./lib/page')(dat)
-	const pageHistory = require('./lib/page-history')(dat)
-	const onError = require('./lib/error')
+const attachApi = (app, dat) => {
+	const routes = {
+		stats: stats(dat),
+		index: index(dat),
+		page: page(dat),
+		pageHistory: pageHistory(dat)
+	}
 
-	const api = express()
-
-	api.use(hsts({maxAge: 24 * 60 * 60 * 1000}))
-	api.use(compression())
-
-	api.use('/static', serve(path.join(__dirname, 'static'), {index: false}))
-
-	api.get('/stats', stats)
-	api.get('/', index)
-	api.get('/:ns', index)
-	api.get('/wiki/:slug', page)
-	api.get('/wiki/:slug/history', pageHistory)
-	api.get('/wiki/Special:History/:slug', pageHistory)
-
-	api.use((err, req, res, next) => {
-		if (process.env.NODE_DEBUG === 'wikipedia-feed-ui') console.error(err)
-		onError(err, req, res, next)
-	})
-
-	return api
+	app.get('/stats', routes.stats)
+	app.get('/', routes.index)
+	app.get('/:ns', routes.index)
+	app.get('/wiki/:slug', routes.page)
+	app.get('/wiki/:slug/history', routes.pageHistory)
+	app.get('/wiki/Special:History/:slug', routes.pageHistory)
 }
 
-module.exports = createApi
+module.exports = attachApi
--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 'use strict'
 
-const openDat = require('dat-node')
+const express = require('express')
 const http = require('http')
+const hsts = require('hsts')
+const compression = require('compression')
+const path = require('path')
+const serve = require('serve-static')
+const openDat = require('dat-node')
 
-const createApi = require('./api')
+const attachApi = require('./api')
+const handleErrors = require('./lib/error')
 
 const startServer = (db, port, opt, cb) => {
-	openDat(db, (err, dat) => {
-		if (err) return cb(err)
-
-		dat.trackStats()
-		if (!opt.offline) dat.joinNetwork()
-
-		const api = createApi(dat)
-		const server = http.createServer(api)
-		server.listen(port, cb)
+	const app = express()
+	const server = http.createServer(app)
+	server.listen(port, (err) => {
+		if (err) cb(err)
 	})
+
+	app.use(hsts({maxAge: 24 * 60 * 60 * 1000}))
+	app.use(compression())
+	app.use('/static', serve(path.join(__dirname, 'static'), {index: false}))
+
+	const startApp = (opt = {}) => {
+		openDat(db, opt, (err, dat) => {
+			if (err) return cb(err)
+
+			dat.trackStats()
+			if (!opt.offline) dat.joinNetwork()
+
+			attachApi(app, dat)
+			app.use(handleErrors)
+		})
+	}
+	startApp()
 }
 
 module.exports = startServer
